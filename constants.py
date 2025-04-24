12f9fb3657adb9364d7820e7df71180c78b01408
Kevin W <kevinwads17@gmail.com>
2025-04-24 12:25:01 -0400

    hitbox alligned

--- constants.py
+++ constants.py
@@ -1,37 +1,32 @@
 import pygame as pg
-from main import POS_CONFIG
-
-ship_pos = POS_CONFIG{ship_pos}
 
 ASSET_CONFIG = {
     "spaceman": {
         "path": "Assets/images/spaceman.png",
         "size": (128, 128),
-        "hitbox": (32, 32, 64, 64),  
+        "hitbox_offset": (32, 32, 64, 64),  
     },
     "asteroid1": {
         "path": "Assets/images/asteroid1.png",
         "size": (256, 256),
-        "hitbox": (40, 40, 176, 176),
+        "hitbox_offset": (40, 40, 176, 176),
     },
-
     "ship": {
         "path": "Assets/images/ship.png",
         "size": (256, 256),
-        "hitbox": (ship_pos, 128, 128),
+        "hitbox_offset": (64, 64, 128, 128),
     },
 }
 
 def load_assets():
     assets = {}
     for name, config in ASSET_CONFIG.items():
-        image_init = pg.image.load(config["path"]).convert_alpha()
-        image_scale = pg.transform.scale(image_init, config["size"])
-        hitbox_rect = pg.Rect(*config["hitbox"])  # unpack tuple into Rect
+        image = pg.image.load(config["path"]).convert_alpha()
+        image_scaled = pg.transform.scale(image, config["size"])
 
         assets[name] = {
-            "image": image_init,
-            "image_size": image_scale,
-            "hitbox": hitbox_rect,
+            "image": image_scaled,
+            "hitbox_offset": config["hitbox_offset"],  # store relative values
         }
     return assets
+
